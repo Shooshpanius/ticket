c1e8aa09cda9e87133971b092f2220d073dc282f
Shooshpanius <admin@in-da-house.ru>
2013-07-16 15:58:19 +0400

    Ticket Manager (test)

--- db/migrate/20130716114517_create_tickets.rb
+++ db/migrate/20130716114517_create_tickets.rb
@@ -2,6 +2,13 @@ class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
 
+      t.integer :initiator_id
+      t.integer :executor_id
+      t.integer :executor_type
+      t.string :topic
+      t.string :text
+      t.string :deadline
+
       t.timestamps
     end
   end
