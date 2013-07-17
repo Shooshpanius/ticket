c86167dd82b4c449cf640a6829481a662b7c4050
Shooshpanius <admin@in-da-house.ru>
2013-07-17 09:38:57 +0400

    Ticket Manager (test)

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,6 +1,7 @@
 class Users < ActiveRecord::Base
 
   has_many :user_by_group, :foreign_key => :users_id, dependent: :destroy
+  has_many :ticket_to_user, :foreign_key => :users_id, dependent: :destroy
   has_many :groups, through: :user_by_group
 
 end
