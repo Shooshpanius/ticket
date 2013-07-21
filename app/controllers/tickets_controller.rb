b9daa668ea663a926aeb7301232ee424071be0e0
Shooshpanius <admin@in-da-house.ru>
2013-07-21 21:47:25 +0400

    Ticket system (test)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,11 +6,11 @@ class TicketsController < ApplicationController
 
   def index
 
-    @user_tickets = TicketToUser.where('users_id = ?', session[:user_id]).limit(10)
+    @user_tickets = TicketToUser.where('users_id = ?', session[:user_id]).limit(10).sort_by{ |elem| elem.deadline }
 
     u = Users.select([:id]).where("id = ?", session[:user_id])
     g = Groups.select([:id]).where("id = ?", u)
-    @group_tickets = TicketToGroup.where("groups_id = ?", g)
+    @group_tickets = TicketToGroup.where("groups_id = ?", g).limit(10).sort_by{ |elem| elem.deadline }
 
     my_tickets_to_users = TicketToUser.where("initiator_id = ?", session[:user_id])
     my_tickets_to_groups = TicketToGroup.where("initiator_id = ?", session[:user_id]).limit(10)
@@ -18,6 +18,23 @@ class TicketsController < ApplicationController
     @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
 
   end
+
+  def in
+    @user_tickets = TicketToUser.where('users_id = ?', session[:user_id]).sort_by{ |elem| elem.deadline }
+    u = Users.select([:id]).where("id = ?", session[:user_id])
+    g = Groups.select([:id]).where("id = ?", u)
+    @group_tickets = TicketToGroup.where("groups_id = ?", g).sort_by{ |elem| elem.deadline }
+  end
+
+  def out
+    my_tickets_to_users = TicketToUser.where("initiator_id = ?", session[:user_id])
+    my_tickets_to_groups = TicketToGroup.where("initiator_id = ?", session[:user_id]).limit(10)
+    my_tickets = my_tickets_to_users + my_tickets_to_groups
+    @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
+  end
+
+
+
 
   def ticket_new
     @users = Users.all
