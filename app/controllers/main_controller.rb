69d00766c07daf8a14331e6b48a5b32032ea5a13
Shooshpanius <admin@in-da-house.ru>
2013-07-21 21:58:34 +0400

    Ticket system (test)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,14 +5,14 @@ class MainController < ApplicationController
 
   def index
 
-    @user_tickets = TicketToUser.where('users_id = ?', session[:user_id]).limit(10).sort_by{ |elem| elem.deadline }
+    @user_tickets = TicketToUser.where('users_id = ? and completed = ?', session[:user_id], false).limit(10).sort_by{ |elem| elem.deadline }
 
     u = Users.select([:id]).where("id = ?", session[:user_id])
     g = Groups.select([:id]).where("id = ?", u)
-    @group_tickets = TicketToGroup.where("groups_id = ?", g).limit(10).sort_by{ |elem| elem.deadline }
+    @group_tickets = TicketToGroup.where("groups_id = ? and completed = ?", g, false).limit(10).sort_by{ |elem| elem.deadline }
 
-    my_tickets_to_users = TicketToUser.where("initiator_id = ?", session[:user_id]).limit(10)
-    my_tickets_to_groups = TicketToGroup.where("initiator_id = ?", session[:user_id]).limit(10)
+    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed = ?", session[:user_id], false)
+    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed = ?", session[:user_id], false).limit(10)
     my_tickets = my_tickets_to_users + my_tickets_to_groups
     @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
 
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,29 +6,29 @@ class TicketsController < ApplicationController
 
   def index
 
-    @user_tickets = TicketToUser.where('users_id = ?', session[:user_id]).limit(10).sort_by{ |elem| elem.deadline }
+    @user_tickets = TicketToUser.where('users_id = ? and completed = ?', session[:user_id], false).limit(10).sort_by{ |elem| elem.deadline }
 
     u = Users.select([:id]).where("id = ?", session[:user_id])
     g = Groups.select([:id]).where("id = ?", u)
-    @group_tickets = TicketToGroup.where("groups_id = ?", g).limit(10).sort_by{ |elem| elem.deadline }
+    @group_tickets = TicketToGroup.where("groups_id = ? and completed = ?", g, false).limit(10).sort_by{ |elem| elem.deadline }
 
-    my_tickets_to_users = TicketToUser.where("initiator_id = ?", session[:user_id])
-    my_tickets_to_groups = TicketToGroup.where("initiator_id = ?", session[:user_id]).limit(10)
+    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed = ?", session[:user_id], false)
+    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed = ?", session[:user_id], false).limit(10)
     my_tickets = my_tickets_to_users + my_tickets_to_groups
     @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
 
   end
 
   def in
-    @user_tickets = TicketToUser.where('users_id = ?', session[:user_id]).sort_by{ |elem| elem.deadline }
+    @user_tickets = TicketToUser.where('users_id = ? and completed = ?', session[:user_id], false).sort_by{ |elem| elem.deadline }
     u = Users.select([:id]).where("id = ?", session[:user_id])
     g = Groups.select([:id]).where("id = ?", u)
-    @group_tickets = TicketToGroup.where("groups_id = ?", g).sort_by{ |elem| elem.deadline }
+    @group_tickets = TicketToGroup.where("groups_id = ? and completed = ?", g, false).sort_by{ |elem| elem.deadline }
   end
 
   def out
-    my_tickets_to_users = TicketToUser.where("initiator_id = ?", session[:user_id])
-    my_tickets_to_groups = TicketToGroup.where("initiator_id = ?", session[:user_id]).limit(10)
+    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed = ?", session[:user_id], false)
+    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed = ?", session[:user_id], false).limit(10)
     my_tickets = my_tickets_to_users + my_tickets_to_groups
     @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
   end
