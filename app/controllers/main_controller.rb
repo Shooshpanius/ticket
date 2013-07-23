9d67107cc836e61c538b98b8571e52cdc0e0bfbe
Shooshpanius <admin@in-da-house.ru>
2013-07-23 09:12:05 +0400

    Ticket Manager (test)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,9 +7,7 @@ class MainController < ApplicationController
 
     @user_tickets = TicketToUser.where('users_id = ? and completed < ?', session[:user_id], 100).limit(10).sort_by{ |elem| elem.deadline }
 
-    u = Users.select([:id]).where("id = ?", session[:user_id])
-    g = Groups.select([:id]).where("id = ?", u)
-    @group_tickets = TicketToGroup.where("groups_id = ? and completed < ?", g, 100).limit(10).sort_by{ |elem| elem.deadline }
+    @group_tickets = TicketToGroup.where("groups_id in (?) and completed < ?", UserByGroup.groups_for_user(session[:user_id]) , 100).limit(10).sort_by{ |elem| elem.deadline }
 
     my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed < ?", session[:user_id], 100)
     my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed < ?", session[:user_id], 100).limit(10)
--- app/models/user_by_group.rb
+++ app/models/user_by_group.rb
@@ -10,4 +10,15 @@ class UserByGroup < ActiveRecord::Base
                                         WHERE user_by_groups.groups_id = #{group_id}")
   end
 
+
+  def UserByGroup.groups_for_user(user_id)
+    groups_query = UserByGroup.find_by_sql("SELECT users_id as users_id, groups_id as id FROM user_by_groups
+
+                                        LEFT JOIN groups ON user_by_groups.id = groups.id
+                                        WHERE user_by_groups.users_id = #{user_id}")
+
+    return groups_query
+  end
+
+
 end
