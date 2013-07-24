6ffd78731385b3413ebbc86c47022e3900ff720c
Shooshpanius <admin@in-da-house.ru>
2013-07-24 15:15:41 +0400

    Ticket Manager (test)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def pass_generate(len=7) chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('1'..'9').to_a - ['o', 'O', 'i', 'I']
+    return Array.new(len) { chars[rand(chars.size)] }.join
+  end
+
 end
