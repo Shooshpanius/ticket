8313845602d5bfdfda58c73070113d3157ba03b6
Shooshpanius <admin@in-da-house.ru>
2013-07-25 15:10:04 +0400

    Ticket Manager (test)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
 
   def pass_generate(len=7) chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('1'..'9').to_a - ['o', 'O', 'i', 'I']
-  return Array.new(len) { chars[rand(chars.size)] }.join
+    return Array.new(len) { chars[rand(chars.size)] }.join
   end
 
 end
