98c16f7b7e5b4975ca6ece142b3af728e40b2c9f
Shooshpanius <admin@in-da-house.ru>
2013-07-25 14:39:42 +0400

    Ticket Manager (test)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -102,9 +102,7 @@ class Admin::UsersController < ApplicationController
 
   private
 
-  def pass_generate(len=7) chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('1'..'9').to_a - ['o', 'O', 'i', 'I']
-    return Array.new(len) { chars[rand(chars.size)] }.join
-  end
+
 
   def is_admin
     if !session[:is_admin]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  def pass_generate(len=7) chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('1'..'9').to_a - ['o', 'O', 'i', 'I']
+  return Array.new(len) { chars[rand(chars.size)] }.join
+  end
+
 end
 
 #
