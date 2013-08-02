ae170bc80606bfb2fef3b4f154511d1ac7b02b3f
Shooshpanius <admin@in-da-house.ru>
2013-08-02 15:06:15 +0400

    Ticket Manager (test)

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ namespace :deploy do
 
   desc "Clear DB"
   task :clear_db , :roles => :app do
-    run "cd #{deploy_to}/current && rake environment RAILS_ENV=production clear_db:vacancy"
+    #run "cd #{deploy_to}/current && rake environment RAILS_ENV=production clear_db:vacancy"
   end
 
   task :precompile_assets, :roles => :app do
