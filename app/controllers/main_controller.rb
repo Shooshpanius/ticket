30ae7c730f130b11c74eef037fb0189524e7be54
Shooshpanius <admin@in-da-house.ru>
2013-08-05 16:53:36 +0400

    Ticket Manager (test)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -12,26 +12,20 @@ class MainController < ApplicationController
     end
     @user_tickets = user_tickets.take(10)
 
-
-
     group_tickets = TicketToGroup.where("group_id in (?) and completed < ?", UserByGroup.groups_for_user(session[:user_id]) , 100).sort_by{ |elem| [elem.actual, elem.deadline]}
     group_tickets.each do |group_ticket|
       group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
     end
     @group_tickets = group_tickets.take(10)
 
-
-
     my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed < ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
     my_tickets_to_users.each do |user_ticket|
       user_ticket[:actual] = ActualTask.is_actual_u(session[:user_id], user_ticket[:id])
     end
-
     my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed < ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
     my_tickets_to_groups.each do |group_ticket|
       group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
     end
-
     my_tickets = my_tickets_to_users + my_tickets_to_groups
     @my_tickets = my_tickets.take(10)
 
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,39 +6,83 @@ class TicketsController < ApplicationController
 
   def index
 
-    @user_tickets = TicketToUser.where('user_id = ? and completed < ?', session[:user_id], 100).limit(10).sort_by{ |elem| elem.deadline }
+    user_tickets = TicketToUser.where('user_id = ? and completed < ?', session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}
+    user_tickets.each do |user_ticket|
+      user_ticket[:actual] = ActualTask.is_actual_u(session[:user_id], user_ticket[:id])
+    end
+    @user_tickets = user_tickets.take(10)
 
-    @group_tickets = TicketToGroup.where("group_id in (?) and completed < ?", UserByGroup.groups_for_user(session[:user_id]) , 100).limit(10).sort_by{ |elem| elem.deadline }
+    group_tickets = TicketToGroup.where("group_id in (?) and completed < ?", UserByGroup.groups_for_user(session[:user_id]) , 100).sort_by{ |elem| [elem.actual, elem.deadline]}
+    group_tickets.each do |group_ticket|
+      group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
+    end
+    @group_tickets = group_tickets.take(10)
 
-    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed < ?", session[:user_id], 100)
-    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed < ?", session[:user_id], 100).limit(10)
+    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed < ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
+    my_tickets_to_users.each do |user_ticket|
+      user_ticket[:actual] = ActualTask.is_actual_u(session[:user_id], user_ticket[:id])
+    end
+    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed < ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
+    my_tickets_to_groups.each do |group_ticket|
+      group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
+    end
     my_tickets = my_tickets_to_users + my_tickets_to_groups
-    @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
+    @my_tickets = my_tickets.take(10)
 
   end
 
   def in
-    @user_tickets = TicketToUser.where('user_id = ? and completed < ?', session[:user_id], 100).sort_by{ |elem| elem.deadline }
-    @group_tickets = TicketToGroup.where("group_id in (?) and completed < ?", UserByGroup.groups_for_user(session[:user_id]) , 100).sort_by{ |elem| elem.deadline }
+    user_tickets = TicketToUser.where('user_id = ? and completed < ?', session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}
+    user_tickets.each do |user_ticket|
+      user_ticket[:actual] = ActualTask.is_actual_u(session[:user_id], user_ticket[:id])
+    end
+    @user_tickets = user_tickets.take(10)
+
+    group_tickets = TicketToGroup.where("group_id in (?) and completed < ?", UserByGroup.groups_for_user(session[:user_id]) , 100).sort_by{ |elem| [elem.actual, elem.deadline]}
+    group_tickets.each do |group_ticket|
+      group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
+    end
+    @group_tickets = group_tickets.take(10)
   end
 
   def out
-    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed < ?", session[:user_id], 100)
-    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed < ?", session[:user_id], 100)
+    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed < ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
+    my_tickets_to_users.each do |user_ticket|
+      user_ticket[:actual] = ActualTask.is_actual_u(session[:user_id], user_ticket[:id])
+    end
+    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed < ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
+    my_tickets_to_groups.each do |group_ticket|
+      group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
+    end
     my_tickets = my_tickets_to_users + my_tickets_to_groups
-    @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
+    @my_tickets = my_tickets.take(10)
   end
 
   def arch_in
-    @user_tickets = TicketToUser.where('user_id = ? and completed = ?', session[:user_id], 100).sort_by{ |elem| elem.deadline }
-    @group_tickets = TicketToGroup.where("group_id in (?) and completed = ?", UserByGroup.groups_for_user(session[:user_id]) , 100).sort_by{ |elem| elem.deadline }
+    user_tickets = TicketToUser.where('user_id = ? and completed = ?', session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}
+    user_tickets.each do |user_ticket|
+      user_ticket[:actual] = ActualTask.is_actual_u(session[:user_id], user_ticket[:id])
+    end
+    @user_tickets = user_tickets.take(10)
+
+    group_tickets = TicketToGroup.where("group_id in (?) and completed = ?", UserByGroup.groups_for_user(session[:user_id]) , 100).sort_by{ |elem| [elem.actual, elem.deadline]}
+    group_tickets.each do |group_ticket|
+      group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
+    end
+    @group_tickets = group_tickets.take(10)
   end
 
   def arch_out
-    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed = ?", session[:user_id], 100)
-    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed = ?", session[:user_id], 100)
+    my_tickets_to_users = TicketToUser.where("initiator_id = ? and completed = ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
+    my_tickets_to_users.each do |user_ticket|
+      user_ticket[:actual] = ActualTask.is_actual_u(session[:user_id], user_ticket[:id])
+    end
+    my_tickets_to_groups = TicketToGroup.where("initiator_id = ? and completed = ?", session[:user_id], 100).sort_by{ |elem| [elem.actual, elem.deadline]}.take(10)
+    my_tickets_to_groups.each do |group_ticket|
+      group_ticket[:actual] = ActualTask.is_actual_g(session[:user_id], group_ticket[:id])
+    end
     my_tickets = my_tickets_to_users + my_tickets_to_groups
-    @my_tickets = my_tickets.sort_by{ |elem| elem.deadline }
+    @my_tickets = my_tickets.take(10)
   end
 
 
