1d78fb4734714c22478057bb390ce04788bf39f1
Shooshpanius <admin@in-da-house.ru>
2013-08-06 14:56:19 +0400

    Ticket Manager (test)

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Ticketmanager
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     #config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Europe/Moscow'
-    config.assets.enabled = false
+    #config.assets.enabled = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
