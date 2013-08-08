69e7ff07f1a76fc2d3ee58a39a12b1556feeee91
Shooshpanius <admin@in-da-house.ru>
2013-08-08 13:58:37 +0400

    Ticket Manager (test)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,17 @@ Ticketmanager::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              '192.168.0.207',
+      port:                 25,
+      #      domain:               '',
+      #      user_name:            '',
+      #      password:             '',
+      #      authentication:       'plain',
+      #      enable_starttls_auto: false
+  }
+
 end
