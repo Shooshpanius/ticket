b6bdf48341b2ba48324b50d65fe492e55f5ff660
Shooshpanius <admin@in-da-house.ru>
2013-08-12 16:53:49 +0400

    Ticket Manager (test)

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,5 +1,8 @@
+# encoding: utf-8
 class TicketMailer < ActionMailer::Base
   default from: "test@velskiyles.ru"
+
+  require "magic_encoding"
 
   def send_welcome_email(user)
     @user = user
@@ -10,26 +13,26 @@ class TicketMailer < ActionMailer::Base
 
   def send_new_group_ticket_to_rctp_email(mail_data)
     @mail_data = mail_data
-    subj = "New ticket for group " + mail_data[:group_name]
+    subj = "Новая заявка на группу " + mail_data[:group_name]
     mail(to: mail_data[:user_email], subject: subj )
   end
 
   def send_new_group_ticket_to_sndr_email(mail_data)
     @mail_data = mail_data
-    subj = "New ticket for group " + mail_data[:group_name]
+    subj = "Ваша заявка на группу " + mail_data[:group_name] + " зарегистрирована"
     mail(to: mail_data[:initiator_email], subject: subj )
   end
 
   def send_new_user_ticket_to_rctp_email(mail_data)
     @mail_data = mail_data
-    subj = "New ticket for user " + mail_data[:user_login]
+    subj = "Новая заявка к исполнению от " + mail_data[:user_login]
     mail(to: mail_data[:user_email], subject: subj )
   end
 
   def send_new_user_ticket_to_sndr_email(mail_data)
     @mail_data = mail_data
-    subj = 'Заявка ' + mail_data[:user_login]
-    mail(to: mail_data[:initiator_email], subject: subj.force_encoding("UTF-8") )
+    subj = 'Ваша заявка пользователю ' + mail_data[:user_login] + " зарегистрирована"
+    mail(to: mail_data[:initiator_email], subject: subj )
   end
 
 
