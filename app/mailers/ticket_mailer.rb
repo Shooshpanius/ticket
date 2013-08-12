09b68a1021765d9d5622e051850782cf198ea2ec
Shooshpanius <admin@in-da-house.ru>
2013-08-12 15:21:29 +0400

    Ticket Manager (test)

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -8,27 +8,10 @@ class TicketMailer < ActionMailer::Base
   end
 
 
-  def send_new_group_ticket_email(ticket)
-    @url  = 'http://web.wood.local/login'
-    @ticket = ticket
-    @group = Group.find(ticket.group_id)
-    @initiator = User.find(ticket.initiator_id)
-    @members = UserByGroup.users_in_group(ticket.group_id)
-    @members.each do |member|
-      @user = User.find(member.user_id)
-      if @user.email.size() > 3
-        @type_email = "rcpt"
-        subj = "New ticket for group " + @group.name.to_s
-        mail(to: @user.email, subject: subj )
-      end
-    end
-
-    if @initiator.email.size() > 3
-      @type_email = "sndr"
-      subj = "New ticket for group " + @group.name.to_s
-      mail(to: @initiator.email, subject: subj )
-    end
-
+  def send_new_group_ticket_to_rctp_email(mail_data)
+    @mail_data = mail_data
+    subj = "New ticket for group " + mail_data[:group_name]
+    mail(to: mail_data[:user_email], subject: subj )
 
 
   end
