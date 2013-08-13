e3414e4948f7f954acdce5bb24eb210fd8291b98
Shooshpanius <admin@in-da-house.ru>
2013-08-13 14:09:43 +0400

    Ticket Manager (test)

---
+++ db/migrate/20130813100651_create_attaches.rb
@@ -0,0 +1,13 @@
+class CreateAttaches < ActiveRecord::Migration
+  def change
+    create_table :attaches do |t|
+
+      t.string :object_type
+      t.integer :object_id
+      t.string :original_filename
+      t.string :filename
+
+      t.timestamps
+    end
+  end
+end
