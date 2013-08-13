f0283329de34f8158192757cfc26cb1c5b1599ad
Shooshpanius <admin@in-da-house.ru>
2013-08-13 09:27:43 +0400

    Ticket Manager (test)

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,11 +1,18 @@
 class LoginController < ApplicationController
 
-
+  require 'digest/md5'
 
 
   def index
 
     @ip = ip()
+
+    user = User.where("auth_hash = ? and auth_last_ip = ?", cookies[:hash], ip() ).take(1)
+    if user.size == 1
+      session[:is_login] = true
+      session[:user_id] = user.id
+      session[:is_admin] = true if user.admin
+    end
 
     if session[:is_login]
       redirect_to '/'
@@ -25,8 +32,15 @@ class LoginController < ApplicationController
       session[:is_login] = true
       session[:user_id] = user.id
 
+      salt = pass_generate(len=7)
+      hash = Digest::MD5.hexdigest(Time.now.to_s + salt.to_s)
+
+      user.auth_hash = hash
       user.auth_last_ip = ip()
       user.save
+
+      cookies[:hash] = { value: hash, expires: 10.hour.from_now }
+
     end
 
     if user.admin
