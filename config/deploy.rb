748a8369d013bc6a5df63edf56ed4757232ee2dd
Shooshpanius <admin@in-da-house.ru>
2013-08-13 16:14:52 +0400

    Ticket Manager (test)

--- config/deploy.rb
+++ config/deploy.rb
@@ -139,7 +139,7 @@ namespace :deploy do
   end
 
   task :link_dependencies, :roles => :app do
-    run "ln  #{shared_path}/attache #{deploy_to}/current/public/attache"
+    run "ln -nfs #{shared_path}/attache #{deploy_to}/current/public/attache"
     #run "ln -nfs #{shared_path}/public/images/posts #{release_path}/public/images/posts"
   end
 
