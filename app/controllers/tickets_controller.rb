b3bea99acd70d13268458ad092e3eec933d28f8b
Shooshpanius <admin@in-da-house.ru>
2013-08-28 09:46:13 +0400

    Ticket Manager (test)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -457,7 +457,7 @@ class TicketsController < ApplicationController
 
   def srv_refactor_tickets_root
 
-    tickets = TicketToGroup.where("root = ?", "NULL")
+    tickets = TicketToGroup.where("root = ?", "")
     tickets.each do |ticket|
       root_array = {
           ticket_type: "g",
@@ -473,7 +473,7 @@ class TicketsController < ApplicationController
     end
 
 
-    tickets = TicketToUser.where("root = ?", "NULL")
+    tickets = TicketToUser.where("root = ?", "")
     tickets.each do |ticket|
       root_array = {
           ticket_type: "u",
