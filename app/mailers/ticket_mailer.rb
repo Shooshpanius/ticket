a29d281e0ded97588850d1a1b8ebf2fd1f905954
Shooshpanius <admin@in-da-house.ru>
2013-08-30 09:57:59 +0400

    Ticket Manager (test)

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -14,56 +14,56 @@ class TicketMailer < ActionMailer::Base
   def send_new_group_ticket_to_rctp_email(mail_data)
     @mail_data = mail_data
     subj = "Новая заявка на группу " + mail_data[:group_name]
-    mail(to: mail_data[:user_email], subject: subj )
+    mail(to: mail_data[:user_email], subject: subj ) if mail_data[:user_email].strip != ""
   end
 
   def send_new_group_ticket_to_sndr_email(mail_data)
     @mail_data = mail_data
     subj = "Заявка \''" + mail_data[:ticket_topic] + "\' зарегистрирована"
-    mail(to: mail_data[:initiator_email], subject: subj )
+    mail(to: mail_data[:initiator_email], subject: subj ) if mail_data[:initiator_email].strip != ""
   end
 
   def send_new_user_ticket_to_rctp_email(mail_data)
     @mail_data = mail_data
     subj = "Новая заявка к исполнению от " + mail_data[:initiator_login]
-    mail(to: mail_data[:user_email], subject: subj )
+    mail(to: mail_data[:user_email], subject: subj ) if mail_data[:user_email].strip != ""
   end
 
   def send_new_user_ticket_to_sndr_email(mail_data)
     @mail_data = mail_data
     subj = 'Ваша заявка пользователю ' + mail_data[:user_login] + " зарегистрирована"
-    mail(to: mail_data[:initiator_email], subject: subj )
+    mail(to: mail_data[:initiator_email], subject: subj ) if mail_data[:initiator_email].strip != ""
   end
 
 
   def send_new_comment_email(mail_data)
     @mail_data = mail_data
     subj = 'Новый комментарий к заявке № ' + @mail_data[:type_comment] + '_' + @mail_data[:ticket_id].to_s
-    mail(to: mail_data[:rcpt_email], subject: subj )
+    mail(to: mail_data[:rcpt_email], subject: subj ) if mail_data[:rcpt_email].strip != ""
   end
 
   def send_change_executor_by_leader(mail_data)
     @mail_data = mail_data
     subj = 'Вы назначены ответственным за выполнение заявки № g_' + @mail_data[:ticket_id].to_s
-    mail(to: mail_data[:rcpt_email], subject: subj )
+    mail(to: mail_data[:rcpt_email], subject: subj ) if mail_data[:rcpt_email].strip != ""
   end
 
   def send_change_executor_by_member(mail_data)
     @mail_data = mail_data
     subj = 'Пользователь ' + @mail_data[:sndr_login] + ' взял ответственность за выполнение заявки № g_' + @mail_data[:ticket_id].to_s
-    mail(to: mail_data[:rcpt_email], subject: subj )
+    mail(to: mail_data[:rcpt_email], subject: subj ) if mail_data[:rcpt_email].strip != ""
   end
 
   def send_change_executor_to_initiator(mail_data)
     @mail_data = mail_data
     subj = 'Назначен ответственный за выполнение вашей заявки № g_' + @mail_data[:ticket_id].to_s
-    mail(to: mail_data[:rcpt_email], subject: subj )
+    mail(to: mail_data[:rcpt_email], subject: subj ) if mail_data[:rcpt_email].strip != ""
   end
 
   def send_change_status(mail_data)
     @mail_data = mail_data
     subj = 'Изменился % выполнения заявки № g_' + @mail_data[:ticket_id].to_s
-    mail(to: mail_data[:rcpt_email], subject: subj )
+    mail(to: mail_data[:rcpt_email], subject: subj ) if mail_data[:rcpt_email].strip != ""
   end
 
 end
