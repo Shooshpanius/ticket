dd63c014fbc0eaf85c60f4796f9327e22993e810
Shooshpanius <admin@in-da-house.ru>
2013-09-03 17:38:00 +0400

    Ticket Manager (test)

--- app/models/ticket_root.rb
+++ app/models/ticket_root.rb
@@ -27,21 +27,39 @@ class TicketRoot < ActiveRecord::Base
 
   def TicketRoot.other_tickets(user_id)
 
-    user_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_users.root as t_root, ticket_to_users.user_id as t_user_id
-                                           FROM ticket_roots
-                                              LEFT JOIN ticket_to_users ON ticket_roots.id = ticket_to_users.root
-                                           WHERE
-                                              ticket_roots.ticket_type = 'u' AND ticket_to_users.user_id != #{user_id}
-                                          ")
+    #user_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_users.root as t_root, ticket_to_users.user_id as t_user_id
+    #                                       FROM ticket_roots
+    #                                          LEFT JOIN ticket_to_users ON ticket_roots.id = ticket_to_users.root
+    #                                       WHERE
+    #                                          ticket_roots.ticket_type = 'u' AND ticket_to_users.user_id != #{user_id}
+    #                                      ")
+
+    my_groups = ""
+    UserByGroup.groups_for_user(user_id).map{|x| x.id}.each do |y|
+        my_groups = my_groups + (y.to_s+",")
+    end
+    my_groups = my_groups + ")"
+
+    my_groups = my_groups.gsub(/(\,\))/,'')
+
+    #if UserByGroup.groups_for_user(user_id).size == 0 then
+    #
+    #  return []
+    #
+    #
+    #end
+
 
     group_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_groups.root as t_root, ticket_to_groups.executor as t_executor
                                             FROM ticket_roots
                                               LEFT JOIN ticket_to_groups ON ticket_roots.id = ticket_to_groups.root
                                             WHERE
-                                            ticket_roots.ticket_type = 'g' AND ticket_to_groups.executor != #{user_id}
+                                            ticket_roots.ticket_type = 'g' AND ticket_to_groups.executor != #{user_id} AND ticket_to_groups.group_id in (#{my_groups})
                                            ")
 
-    return user_tickets + group_tickets
+    return group_tickets
   end
 
 end
+
+# AND ticket_to_groups.group_id IN #{my_groups}
