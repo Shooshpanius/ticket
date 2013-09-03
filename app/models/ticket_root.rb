00293f4c73997dc7b63d47aa5c3cd7f7f441136a
Shooshpanius <admin@in-da-house.ru>
2013-09-03 14:47:16 +0400

    Ticket Manager (test)

--- app/models/ticket_root.rb
+++ app/models/ticket_root.rb
@@ -11,31 +11,48 @@ class TicketRoot < ActiveRecord::Base
 
 
 
-    user_tickets = TicketRoot.find_by_sql("SELECT
-                                            ticket_roots.*,
-                                            ticket_to_users.id as t_id,
-                                            ticket_to_users.initiator_id  as t_initiator_id,
-                                            ticket_to_users.user_id  as t_user_id,
-                                            ticket_to_users.topic  as t_topic,
-                                            ticket_to_users.text  as t_text,
-                                            ticket_to_users.deadline  as t_deadline,
-                                            ticket_to_users.completed  as t_completed,
-                                            ticket_to_users.actual  as t_actual,
-                                            ticket_to_users.created_at  as t_created_at,
-                                            ticket_to_users.updated_at  as t_updated_at,
-                                            ticket_to_users.root  as t_root
+    #user_tickets = TicketRoot.find_by_sql("SELECT
+    #                                        ticket_roots.*,
+    #                                        ticket_to_users.id as t_id,
+    #                                        ticket_to_users.initiator_id  as t_initiator_id,
+    #                                        ticket_to_users.user_id  as t_user_id,
+    #                                        ticket_to_users.topic  as t_topic,
+    #                                        ticket_to_users.text  as t_text,
+    #                                        ticket_to_users.deadline  as t_deadline,
+    #                                        ticket_to_users.completed  as t_completed,
+    #                                        ticket_to_users.actual  as t_actual,
+    #                                        ticket_to_users.created_at  as t_created_at,
+    #                                        ticket_to_users.updated_at  as t_updated_at,
+    #                                        ticket_to_users.root  as t_root
+    #                                       FROM ticket_roots
+    #                                          LEFT JOIN ticket_to_users ON ticket_roots.id = ticket_to_users.root
+    #                                       WHERE
+    #                                          ticket_roots.ticket_type = 'u' AND ticket_to_users.user_id = #{user_id}
+    #                                       ")
+    #
+    #
+    #group_tickets = TicketRoot.find_by_sql("SELECT * FROM ticket_roots
+    #                                    LEFT JOIN ticket_to_groups ON ticket_roots.id = ticket_to_groups.root
+    #                                    WHERE
+    #                                      (ticket_roots.ticket_type = 'g' AND ticket_to_groups.executor = #{user_id})
+    #                                    ")
+
+
+    user_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_users.root as t_root, ticket_to_users.user_id as t_user_id
                                            FROM ticket_roots
                                               LEFT JOIN ticket_to_users ON ticket_roots.id = ticket_to_users.root
                                            WHERE
                                               ticket_roots.ticket_type = 'u' AND ticket_to_users.user_id = #{user_id}
-                                           ")
+                                          ")
 
 
-    group_tickets = TicketRoot.find_by_sql("SELECT * FROM ticket_roots
-                                        LEFT JOIN ticket_to_groups ON ticket_roots.id = ticket_to_groups.root
-                                        WHERE
-                                          (ticket_roots.ticket_type = 'g' AND ticket_to_groups.executor = #{user_id})
-                                        ")
+    group_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_groups.root as t_root, ticket_to_groups.executor as t_executor
+                                            FROM ticket_roots
+                                              LEFT JOIN ticket_to_groups ON ticket_roots.id = ticket_to_groups.root
+                                            WHERE
+                                            ticket_roots.ticket_type = 'g' AND ticket_to_groups.executor = #{user_id}
+                                            ")
+
 
 
     return user_tickets + group_tickets
