010ab8905797527490311497aed65168183e8812
Shooshpanius <admin@in-da-house.ru>
2013-09-04 09:03:00 +0400

    Ticket Manager (test)

--- app/models/ticket_root.rb
+++ app/models/ticket_root.rb
@@ -7,48 +7,55 @@ class TicketRoot < ActiveRecord::Base
 
   def TicketRoot.my_tickets(user_id)
 
-    user_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_users.root as t_root, ticket_to_users.user_id as t_user_id
+    user_tickets = TicketRoot.find_by_sql("SELECT
+                                            ticket_roots.*,
+                                            ticket_to_users.root as t_root,
+                                            ticket_to_users.user_id as t_user_id,
+                                            ticket_to_users.actual as actual
                                            FROM ticket_roots
                                               LEFT JOIN ticket_to_users ON ticket_roots.id = ticket_to_users.root
                                            WHERE
                                               ticket_roots.ticket_type = 'u' AND ticket_to_users.user_id = #{user_id} AND ticket_to_users.completed != '100'
                                           ")
 
-    group_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_groups.root as t_root, ticket_to_groups.executor as t_executor
+    group_tickets = TicketRoot.find_by_sql("SELECT
+                                              ticket_roots.*,
+                                              ticket_to_groups.root as t_root,
+                                              ticket_to_groups.executor as t_executor,
+                                              ticket_to_groups.actual as actual
                                             FROM ticket_roots
                                               LEFT JOIN ticket_to_groups ON ticket_roots.id = ticket_to_groups.root
                                             WHERE
                                             ticket_roots.ticket_type = 'g' AND ticket_to_groups.executor = #{user_id} AND ticket_to_groups.completed != '100'
                                             ")
 
-    return user_tickets + group_tickets
+    my_tickets = user_tickets + group_tickets
+
+    my_tickets.each do |my_ticket|
+      my_ticket[:actual] = (my_ticket.ticket_type == "g") ? ActualTask.is_actual_g(user_id, my_ticket.ticket_id) : ActualTask.is_actual_u(user_id, my_ticket.ticket_id)
+    end
+    my_tickets.sort! do |a, b|
+      (b.actual <=> a.actual).nonzero? ||
+          (b.created_at <=> a.created_at)
+    end
+
+
+
+    return my_tickets
   end
 
 
   def TicketRoot.other_tickets(user_id)
-
-    #user_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_users.root as t_root, ticket_to_users.user_id as t_user_id
-    #                                       FROM ticket_roots
-    #                                          LEFT JOIN ticket_to_users ON ticket_roots.id = ticket_to_users.root
-    #                                       WHERE
-    #                                          ticket_roots.ticket_type = 'u' AND ticket_to_users.user_id != #{user_id}
-    #                                      ")
 
     my_groups = ""
     UserByGroup.groups_for_user(user_id).map{|x| x.id}.each do |y|
         my_groups = my_groups + (y.to_s+",")
     end
     my_groups = my_groups + ")"
-
     my_groups = my_groups.gsub(/(\,\))/,'')
-
     if UserByGroup.groups_for_user(user_id).size == 0 then
-
       return []
-
-
     end
-
 
     group_tickets = TicketRoot.find_by_sql("SELECT ticket_roots.*, ticket_to_groups.root as t_root, ticket_to_groups.executor as t_executor
                                             FROM ticket_roots
@@ -58,6 +65,7 @@ class TicketRoot < ActiveRecord::Base
                                            ")
 
     return group_tickets
+
   end
 
 end
