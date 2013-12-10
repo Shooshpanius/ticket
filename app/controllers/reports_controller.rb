bed7afa02dde227440807e7f52ea0814ddd346b7
Shooshpanius <admin@in-da-house.ru>
2013-12-10 13:39:18 +0400

    Ticket Manager (test)

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -14,14 +14,10 @@ class ReportsController < ApplicationController
   def srv_activity
 
 
-    #user_comments = TicketComment.where('user_id = (?)',  session[:user_id] )
-    #user_comments = TicketComment.where('created_at >= (?) and created_at <= (?) and user_id = (?)', params[:inputDateFrom].to_time.to_i, params[:inputDateTo].to_time.to_i, session[:user_id] )
     user_comments = TicketComment.where('created_at >= (?) and created_at <= (?) and user_id = (?)', params[:inputDateFrom].to_time.to_datetime, params[:inputDateTo].to_time.to_datetime, session[:user_id] )
 
     @form_data = {
-      user_comments: user_comments,
-      inputDateFrom: params[:inputDateFrom].to_time.to_datetime ,
-      inputDateTo: params[:inputDateTo].to_time.to_datetime
+      user_comments: user_comments
     }
 
     render(:layout => false)
