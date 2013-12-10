fddc0c71964293ff15c9eab54cb86da37adda899
Shooshpanius <admin@in-da-house.ru>
2013-12-10 09:04:56 +0400

    Ticket Manager (test)

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,23 @@
+class ReportsController < ApplicationController
+
+  before_filter :is_login
+
+
+  def activity
+
+
+
+  end
+
+
+
+
+
+  private
+  def is_login
+    unless session[:is_login]
+      redirect_to "/"
+    end
+  end
+
+end
