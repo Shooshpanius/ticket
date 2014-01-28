b5682f85ff10dd3ee98f60acfbc0194b7eecbfb1
Shooshpanius <admin@in-da-house.ru>
2014-01-28 17:32:50 +0400

    Ticket Manager (test)

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -12,16 +12,6 @@ class PlanController < ApplicationController
 
   def records
 
-    #my_tickets = TicketRoot.my_tickets(session[:user_id])
-    #other_tickets = TicketRoot.other_tickets(session[:user_id])
-    #out_tickets = TicketRoot.out_tickets(session[:user_id])
-    #
-    #@form_data = {
-    #    my_tickets: my_tickets,
-    #    other_tickets: other_tickets,
-    #    out_tickets: out_tickets
-    #}
-
     my_tickets = Plan.where('recipient_id = ?', session[:user_id])
     @form_data = {
         my_tickets: my_tickets
